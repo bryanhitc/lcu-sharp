af1414a7e080868749bf78cd5316b643a34ee8d6
Bryan Hitchcock <bryanhitc@gmail.com>
2021-03-06 02:02:47 -0800

    Add missing .ConfigureAwait(false)
    Eliminates unneccesary context switches

--- LCUSharp/Utility/LockFileHandler.cs
+++ LCUSharp/Utility/LockFileHandler.cs
@@ -29,7 +29,7 @@ namespace LCUSharp.Utility
             {
                 using (var reader = new StreamReader(fileStream))
                 {
-                    var contents = await reader.ReadToEndAsync();
+                    var contents = await reader.ReadToEndAsync().ConfigureAwait(false);
                     var items = contents.Split(':');
 
                     var processId = int.Parse(items[1]);
@@ -70,7 +70,7 @@ namespace LCUSharp.Utility
             fileWatcher.Created += OnFileCreated;
             fileWatcher.EnableRaisingEvents = true;
 
-            await fileCreated.Task;
+            await fileCreated.Task.ConfigureAwait(false);
             return filePath;
         }
     }
